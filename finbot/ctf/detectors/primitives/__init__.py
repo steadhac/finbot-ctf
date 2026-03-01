6a7ffba7fbd36f8a4388a985e4cfe19a033c0251
Venkata Sai Kishore Modalavalasa <sai.kishu@gmail.com>
2026-03-01 00:30:04 -0800

    PromptInjection, Jailbreak primitive

--- finbot/ctf/detectors/primitives/__init__.py
+++ finbot/ctf/detectors/primitives/__init__.py
@@ -1,6 +1,7 @@
 """Detector Primitives"""
 
 from finbot.ctf.detectors.primitives.pattern_match import PatternMatchDetector
+from finbot.ctf.detectors.primitives.pi_jb import PromptInjectionDetector
 from finbot.ctf.detectors.primitives.tool_call import ToolCallDetector
 
-__all__ = ["PatternMatchDetector", "ToolCallDetector"]
+__all__ = ["PatternMatchDetector", "PromptInjectionDetector", "ToolCallDetector"]
